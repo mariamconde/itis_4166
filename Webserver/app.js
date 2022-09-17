9f327ec9e5ac8db69db2f25236f5539d45077b59
mariamconde <condemriam@gmail.com>
2022-09-16 21:39:07 -0400

    create a response

--- Webserver/app.js
+++ Webserver/app.js
@@ -1,15 +1,28 @@
 //require the http module
 const http = require('http');
+const fs = require('fs');
 const port = 8084;
 const host = 'localhost';
 
 //create server using required module
 const server = http.createServer((req, res) => {
-    console.log('method', req.method);
-    // url / - landing page
-    console.log('url', req.url);
-    
-    console.log('header', req.headers);
+    // console.log('method', req.method);
+    // // url / - landing page
+    // console.log('url', req.url);
+    // console.log('header', req.headers);
+
+    //response
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('./index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
 });
 // above is more concis
 //listens on particular port
