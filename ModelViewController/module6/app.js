5daad7192ed1e747992db6ef867e29642636efe1
mariamconde <condemriam@gmail.com>
2022-10-03 13:49:43 -0400

    set up MVC skeleton

---
+++ ModelViewController/module6/app.js
@@ -0,0 +1,28 @@
+//require modules
+const express = require('express');
+const morgan = require('morgan');
+
+
+//create app
+const app = express();
+
+//configure app
+let port = 3000;
+let host = 'localhost';
+app.set('view engine', 'ejs');
+
+//mount middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+
+//set up routes
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+
+
+//start the server
+app.listen(port, host, () => {
+    console.log('Server is running on port', port)
+})
