189d41b0dd193ef670dd9463f6097cd17a6fbbfa
mariamconde <condemriam@gmail.com>
2022-10-28 16:42:15 -0400

    refactor the app for showing story details

--- Module8/controllers/storyController.js
+++ Module8/controllers/storyController.js
@@ -25,15 +25,22 @@ exports.create = (req, res, next) => {
 
 exports.show = (req, res, next) => {
     let id = req.params.id;
-    let story = model.findById(id);
-    if (story) {
-        res.render('./story/show', { story });
-    } else {
-        let err = new Error('Cannot find a story with id ' + id);
-        err.status = 404;
-        next(err);
+    //an objectId is 24-bit Hex string
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+        let err = new Error('Invalid story id');
+        err.status = 400;
+        return next(err);
     }
-
+    model.findById(id)
+        .then(story => {
+            if (story) {
+                res.render('./story/show', { story });
+            } else {
+                let err = new Error('Cannot find a story with id ' + id);
+                next(err);
+            }
+        })
+        .catch(err => next(err));
 };
 
 exports.edit = (req, res, next) => {
